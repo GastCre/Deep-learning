29bd336ea7b93f99e3cf366dde8a233705dc2708
GastCre <gcrecike@gmail.com>
2026-03-06 15:15:03 +0100

    Implement ImageNet and MNIST dataset loaders; add training loop and visualization for AlexNet

--- Architectures/AlexNet/AlexNet_NN.py
+++ Architectures/AlexNet/AlexNet_NN.py
@@ -1,22 +1,30 @@
-# %% packages
+# %% Adding the system path to import the dataset module
 import torch
 import torchvision
 import torchvision.transforms as transforms
 import torch.nn as nn
-import os
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 import numpy as np
+from data.ImageNet.dataset_ImageNet import trainloader, testloader, validationloader
+import seaborn as sns
+import os
+os.chdir("/Users/gastoncrecikeinbaum/Documents/Data Science/Courses/Deep learning")
+
+
 # %%
 
 
 class AlexNet(nn.Module):
     def __init__(self) -> None:
         super().__init__()
+        # Out_channels = (in_channels - kernel_size) / stride + 1
+        # First layer out_channel is a design choice
         self.conv1 = nn.Conv2d(
             in_channels=3, out_channels=96, kernel_size=11, stride=4)
         self.maxpool1 = nn.MaxPool2d(kernel_size=3, stride=2)
         self.conv2 = nn.Conv2d(
+            # Out_channels = 96 - 5 + 2*2 +1 = 96
             in_channels=96, out_channels=256, kernel_size=5, padding=2)
         self.maxpool2 = nn.MaxPool2d(kernel_size=3, stride=2)
         self.conv3 = nn.Conv2d(
@@ -27,7 +35,7 @@ class AlexNet(nn.Module):
             in_channels=384, out_channels=256, kernel_size=3, padding=1)
         self.maxpool3 = nn.MaxPool2d(kernel_size=3, stride=2)
         self.flatten = nn.Flatten()
-        self.fc1 = nn.Linear(in_features=256*6*6, out_features=4096)
+        self.fc1 = nn.Linear(in_features=256*5*5, out_features=4096)
         self.dropout1 = nn.Dropout(p=0.5)
         self.fc2 = nn.Linear(in_features=4096, out_features=4096)
         self.dropout2 = nn.Dropout(p=0.5)
@@ -58,5 +66,39 @@ class AlexNet(nn.Module):
         x = self.dropout2(x)
         x = self.fc3(x)
         # Remove if using CrossEntropyLoss which applies softmax internally
-        x = self.softmax(x)
+        # x = self.softmax(x)
         return x
+
+
+# %% Training the model
+device = torch.device("mps" if torch.backends.mps.is_available() else "cpu")
+model = AlexNet().to(device)
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+losses = []
+NUM_EPOCHS = 10
+for epoch in range(NUM_EPOCHS):
+    loss_epochs = []
+    for i, batch in enumerate(trainloader, 0):
+        inputs, labels = batch['image'], batch['label']
+        inputs, labels = inputs.to(device), labels.to(device)
+        # zero gradients
+        optimizer.zero_grad()
+        # forward pass
+        outputs = model(inputs)
+        # calculate loss
+        loss = loss_fn(outputs, labels)
+        # backward pass
+        loss.backward()
+        # update weights
+        optimizer.step()
+        loss_epochs.append(loss.item())
+        print(
+            f"Epoch {epoch+1}/{NUM_EPOCHS}, Batch {i+1}/{len(trainloader)}, Loss: {loss.item():.4f}")
+    losses.append(float(loss.item()))
+
+# %% Visualize the training loss
+sns.lineplot(x=range(NUM_EPOCHS), y=losses)
+
+# %%
---
+++ data/MNIST_1D/dataset_MNIST.py
@@ -0,0 +1,33 @@
+# %% Imports
+from datasets import get_dataset_split_names, load_dataset
+from torch.utils.data import DataLoader
+import torchvision.transforms as transforms
+
+# %% Transforms
+
+transform = transforms.Compose([
+    # transforms.ToPILImage(),  # Convert tensor to PIL Image
+    # We use 227x227 instead of 224x224 as noticed by Karpathy (see Wikipedia entry of AlexNet)
+    transforms.Resize((227, 227)),
+    transforms.ToTensor(),    # Back to tensor, now shape [1, 227, 227]
+    transforms.Lambda(lambda x: x.repeat(3, 1, 1)),  # Convert 1 channel to 3
+])
+
+
+def apply_transform(batch):
+    batch['image'] = [transform(img) for img in batch['image']]
+    return batch
+
+
+# %%
+ds = load_dataset("ylecun/mnist")
+ds = ds.with_format("torch")
+ds = ds.with_transform(apply_transform)
+split = get_dataset_split_names("ylecun/mnist")
+ds_train = ds[split[0]]
+ds_test = ds[split[1]]
+# %%
+BATCH_SIZE = 4
+trainloader = DataLoader(ds_train, batch_size=BATCH_SIZE, shuffle=True)
+testloader = DataLoader(ds_test, batch_size=BATCH_SIZE, shuffle=False)
+# %%
