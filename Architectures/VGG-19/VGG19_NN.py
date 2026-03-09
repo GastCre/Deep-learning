db7e999e0e41d4d949388320b7939205484f5f52
GastCre <gcrecike@gmail.com>
2026-03-09 11:37:15 +0100

    Add additional convolutional layers, pooling, and fully connected layers to VGG19 architecture

--- Architectures/VGG-19/VGG19_NN.py
+++ Architectures/VGG-19/VGG19_NN.py
@@ -28,3 +28,38 @@ class VGG19(nn.Module):
         self.conv2_2 = nn.Conv2d(
             in_channels=128, out_channels=128, kernel_size=3, padding=1)
         self.maxpool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv3_1 = nn.Conv2d(
+            in_channels=128, out_channels=256, kernel_size=3, padding=1)
+        self.conv3_2 = nn.Conv2d(
+            in_channels=256, out_channels=256, kernel_size=3, padding=1)
+        self.conv3_3 = nn.Conv2d(
+            in_channels=256, out_channels=256, kernel_size=3, padding=1)
+        self.conv3_4 = nn.Conv2d(
+            in_channels=256, out_channels=256, kernel_size=3, padding=1)
+        self.maxpool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv4_1 = nn.Conv2d(
+            in_channels=256, out_channels=512, kernel_size=3, padding=1)
+        self.conv4_2 = nn.Conv2d(
+            in_channels=512, out_channels=512, kernel_size=3, padding=1)
+        self.conv4_3 = nn.Conv2d(
+            in_channels=512, out_channels=512, kernel_size=3, padding=1)
+        self.conv4_4 = nn.Conv2d(
+            in_channels=512, out_channels=512, kernel_size=3, padding=1)
+        self.maxpool4 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.conv5_1 = nn.Conv2d(
+            in_channels=512, out_channels=512, kernel_size=3, padding=1)
+        self.conv5_2 = nn.Conv2d(
+            in_channels=512, out_channels=512, kernel_size=3, padding=1)
+        self.conv5_3 = nn.Conv2d(
+            in_channels=512, out_channels=512, kernel_size=3, padding=1)
+        self.conv5_4 = nn.Conv2d(
+            in_channels=512, out_channels=512, kernel_size=3, padding=1)
+        self.maxpool5 = nn.MaxPool2d(kernel_size=2, stride=2)
+        self.flatten = nn.Flatten()
+        self.fc1 = nn.Linear(in_features=512*7*7, out_features=4096)
+        self.dropout1 = nn.Dropout(p=0.5)
+        self.fc2 = nn.Linear(in_features=4096, out_features=4096)
+        self.dropout2 = nn.Dropout(p=0.5)
+        self.fc3 = nn.Linear(in_features=4096, out_features=10)
+        self.relu = nn.ReLU()
+        self.softmax = nn.Softmax(dim=1)
